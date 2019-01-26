d4cdfcf30b9f3cf53ad3c6162f6fc450a3450102
SparksFullStack <csparks@antioch.edu>
2019-01-25 17:14:24 -0800

    categories being loaded dynamically from the database

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -38,31 +38,35 @@
 
                 <!-- Blog Categories Well -->
                 <div class="well">
+
+                    <?php
+                        $query = "
+                            SELECT * FROM categories
+                        ";
+                        $categories_query = mysqli_query($connection, $query);
+                    ?>
+
                     <h4>Blog Categories</h4>
                     <div class="row">
                         <div class="col-lg-6">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+                                <?php
+                                    while ($row = mysqli_fetch_assoc($categories_query)){
+                                        $cat_title = $row['cat_title']; // here cat_title is the name of the field in the database
+                                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                                    }
+                                ?>
                             </ul>
                         </div>
                         <!-- /.col-lg-6 -->
                         <div class="col-lg-6">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+                                <?php
+                                    while ($row = mysqli_fetch_assoc($categories_query)){
+                                        $cat_title = $row['cat_title']; // here cat_title is the name of the field in the database
+                                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                                    }
+                                ?>
                             </ul>
                         </div>
                         <!-- /.col-lg-6 -->
