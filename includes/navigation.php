011af3823277cdc51e3b752013f071bdc332bdb3
SparksFullStack <csparks@antioch.edu>
2019-01-25 17:50:33 -0800

    started making admin code modular--have done the header and footer so far

--- includes/navigation.php
+++ includes/navigation.php
@@ -34,6 +34,8 @@
                             echo "<li><a href='#'>{$cat_title}</a></li>";
                         }
                     ?>
+
+                    <li><a href="./admin">Admin</a></li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
